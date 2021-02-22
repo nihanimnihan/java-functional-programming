b3987fafc7178450c39aa1db8adc54acbb7c36ab
nihanim <nihanimnihan@gmail.com>
2021-02-22 21:37:38 +0300

    Functional Interfaces

---
+++ src/programming/FP03BehaviorParameterization.java
@@ -0,0 +1,34 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class FP03BehaviorParameterization {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(1,2,3,4,5,6);
+
+        filterAndPrint(numbers, x -> x % 2 == 0);
+        filterAndPrint(numbers, x -> x % 2 != 0);
+
+        mappingAndPrint(numbers, x -> x * x);
+        mappingAndPrint(numbers, x -> x * x * x);
+
+        mappingAndCreateNewList(numbers, x -> x * x);
+        mappingAndCreateNewList(numbers, x -> x * x * x);
+    }
+
+    private static void filterAndPrint(List<Integer> numbers, Predicate<Integer> predicate) {
+        numbers.stream().filter(predicate).forEach(System.out::println);
+    }
+    private static void mappingAndPrint(List<Integer> numbers, Function<Integer, Integer> function) {
+        numbers.stream().map(function).forEach(System.out::println);
+    }
+
+    private static List<Integer> mappingAndCreateNewList(List<Integer> numbers, Function<Integer, Integer> function) {
+        return numbers.stream().map(function).collect(Collectors.toList());
+    }
+}
--- src/programming/FP03FunctionalInterfaces.java
+++ src/programming/FP03FunctionalInterfaces.java
@@ -1,6 +1,7 @@
 package programming;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,10 +13,45 @@ public class FP03FunctionalInterfaces {
         List<Integer> numbers = List.of(1,2,3,4,5,6);
 
         Predicate<Integer> isEvenPredicate = number -> number % 2 == 0;
+
+        Predicate<Integer> isEventPredicate2 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer % 2 == 0;
+            }
+        };
+
         Function<Integer, Integer> squareFunction = number -> number * number;
+
+        Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer * integer;
+            }
+        };
+
         Consumer<Integer> sysOutConsumer = System.out::println;
 
+        Consumer<Integer> sysOutConsumer2 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+
         numbers.stream().filter(isEvenPredicate).map(squareFunction).forEach(sysOutConsumer);
+
+        BinaryOperator<Integer> sumBinaryOperator = Integer::sum;
+
+        BinaryOperator<Integer> sumBinaryOperator2 = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer a, Integer b) {
+                return a + b;
+            }
+        };
+
+        int sum = numbers.stream().reduce(0, sumBinaryOperator);
+
 
     }
 }
