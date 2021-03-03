b669bce83db82247d4390a9f7fae8e5a1ce76cb2
nihanim <nihanimnihan@gmail.com>
2021-03-03 18:16:24 +0300

    FP05Parallelization added

---
+++ src/programming/FP05Parallelization.java
@@ -0,0 +1,20 @@
+package programming;
+
+import java.util.stream.LongStream;
+
+class FP05Parallelization {
+
+    public static void main(String[] args) {
+
+        long time = System.currentTimeMillis();
+
+        System.out.println(LongStream.range(0, 1000000000).sum());
+        System.out.println(System.currentTimeMillis() - time);
+
+        long time2 = System.currentTimeMillis();
+
+        System.out.println(LongStream.range(0, 1000000000).parallel().sum());
+        System.out.println(System.currentTimeMillis() - time2);
+    }
+
+}
---
+++ src/programming/FP06Files.java
@@ -0,0 +1,14 @@
+package programming;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FP06Files {
+
+    public static void main(String[] args) throws IOException {
+        Files.list(Paths.get(".")).filter(Files::isDirectory).forEach(System.out::println);
+    }
+
+
+}
